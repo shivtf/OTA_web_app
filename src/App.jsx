aaec3188bf20ceb031f6a6375e2a2244ed2da9d2
Akshat Kumar Nautiyal <128883890+Mugiwara0701@users.noreply.github.com>
2026-06-10 12:26:46 +0530

    Browser Router fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Dashboard from "./component/Dashboard/dashboard";
 import FlightsPage from "./component/Flight/flightPage";
 import FlightResults from "./component/Flight/flightResult";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/flights" element={<FlightsPage />} />
-      <Route path="/flights/results" element={<FlightResults />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/flights" element={<FlightsPage />} />
+        <Route path="/flights/results" element={<FlightResults />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
