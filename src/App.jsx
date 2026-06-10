ea4a3048114c6c8996a1b79a5fc04f11a04c00ab
Shivam Bhandari <168291459+shivtf@users.noreply.github.com>
2026-06-10 15:40:19 +0530

    login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Dashboard from "./component/Dashboard/dashboard";
 import FlightsPage from "./component/Flight/flightPage";
 import FlightResults from "./component/Flight/flightResult";
+import Login from "./component/Auth/login";
+import Register from "./component/Auth/register";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/flights" element={<FlightsPage />} />
         <Route path="/flights/results" element={<FlightResults />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
